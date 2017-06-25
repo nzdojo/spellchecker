90f1f7ef42b172cd020511161218665cdc2abfc5
Teamzone <teamzone@aboutagile.com>
2017-06-25 16:46:30 +0000

    Setup the test fixture with a failing

---
+++ spellCheckerTests.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var assert = require('assert'); 
+
+describe("Spell Checker Tests", function () {
+
+    it('Failing Test', function (done) {
+
+        assert(false, 'Failing Test');
+        
+        done();
+    });
+
+});
