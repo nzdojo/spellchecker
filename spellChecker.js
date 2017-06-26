822e8fa7212a6ffeab2e5ff538cb550f56017edc
Teamzone <teamzone@aboutagile.com>
2017-06-26 01:21:00 +0000

    Check for emptyness, not that valuable it seems, feel confident to change the algorithm under that covers now

--- spellChecker.js
+++ spellChecker.js
@@ -1,27 +1,18 @@
-var BitSet = require("bitset.js");
+function SpellChecker(words) {
+    this.words = words;
+}
 
-var SpellChecker = (function () {
-    
-    function SpellChecker(words) {
-        this.words = words;
-        this.bs = new BitSet();
+SpellChecker.prototype.check = function (toCheck) {
+    if (toCheck) {
+        return this.words.indexOf(toCheck.toLowerCase()) > -1;
     }
+    else {
+        return true;
+    }
+};
     
-    SpellChecker.prototype.check = function (toCheck) {
-        if (toCheck) {
-            return this.words.indexOf(toCheck.toLowerCase()) > -1;
-        }
-        else {
-            return true;
-        }
-    };
+SpellChecker.prototype.isDictionaryEmpty = function () {
+    return this.words.length < 1;
+};
     
-    this.isDictionaryEmpty = function () {
-        return this.words.length> 0;
-    };
-    
-    return SpellChecker;
-    
-}());
-
 module.exports = SpellChecker;
