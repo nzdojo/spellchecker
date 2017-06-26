4b69df89330f456b91d2635c190d1fd699a8deb5
Teamzone <teamzone@aboutagile.com>
2017-06-26 01:43:08 +0000

    Bitmap Hash Created, some refactoring of constructor.  Looks crude but could it be enough.  For small dictionaries it is overkill so it should be tested with bigger dictionaries for storage and retrieval savings

--- spellChecker.js
+++ spellChecker.js
@@ -4,17 +4,18 @@ var HashStrategy = require('./hashStrategy');
 function SpellChecker(words) {
     this.hashSetAllWords = new BitSet;
     this.hashStrategy = new HashStrategy();
-    for (var i = words.length - 1; i >= 0; i--) {
-        var hashIndices = this.hashStrategy.hashIndices(words[i].toLowerCase());
-        for (var j = 0; j < hashIndices.length; j++) {
-            this.hashSetAllWords.set(hashIndices[j], 1);   
-        }
-    }    
+    createTheHashDictionarySoWordsCanDiscarded(this.hashSetAllWords, this.hashStrategy, words);
 }
 
 SpellChecker.prototype.check = function (toCheck) {
     if (toCheck) {
-        return this.words.indexOf(toCheck.toLowerCase()) > -1;
+        var hashIndices = this.hashStrategy.hashIndices(toCheck.toLowerCase());
+        // a simple three index hash, can you tell from the code itself?
+        var hash1Set = this.hashSetAllWords.get(hashIndices[0]); 
+        var hash2Set = this.hashSetAllWords.get(hashIndices[1]);
+        var hash3Set = this.hashSetAllWords.get(hashIndices[2]);
+        // very crude test for equivalence.  All need to be set.  2 out out 3 is bad
+        return hash1Set && hash2Set && hash3Set;
     }
     else {
         return true;
@@ -24,5 +25,14 @@ SpellChecker.prototype.check = function (toCheck) {
 SpellChecker.prototype.isDictionaryEmpty = function () {
     return this.hashSetAllWords.isEmpty();
 };
+
+function createTheHashDictionarySoWordsCanDiscarded(hashSetAllWords, hashStrategy, words) {
+    for (var i = words.length - 1; i >= 0; i--) {
+        var hashIndices = hashStrategy.hashIndices(words[i].toLowerCase());
+        for (var j = 0; j < hashIndices.length; j++) {
+            hashSetAllWords.set(hashIndices[j], 1);   
+        }
+    }  
+}
     
 module.exports = SpellChecker;
